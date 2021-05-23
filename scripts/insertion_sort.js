7193ebb432cdca777be1cb2c4f386465250b011d
Shambhawi Bhure <63147153+ShambhawiBhure@users.noreply.github.com>
2021-05-23 14:05:09 +0530

    Update insertion_sort.js

--- scripts/insertion_sort.js
+++ scripts/insertion_sort.js
@@ -4,28 +4,28 @@ function Insertion()
 
     for(var j=0;j<array_size;j++)
     {
-        div_update(divs[j],div_sizes[j],"yellow");//Color update
+        div_update(divs[j],div_sizes[j],"#ffcc29");//Color update
 
         var key= div_sizes[j];
         var i=j-1;
         while(i>=0 && div_sizes[i]>key)
         {
-            div_update(divs[i],div_sizes[i],"red");//Color update
-            div_update(divs[i+1],div_sizes[i+1],"red");//Color update
+            div_update(divs[i],div_sizes[i],"#ce1212");//Color update
+            div_update(divs[i+1],div_sizes[i+1],"#ce1212");//Color update
 
             div_sizes[i+1]=div_sizes[i];
 
-            div_update(divs[i],div_sizes[i],"red");//Height update
-            div_update(divs[i+1],div_sizes[i+1],"red");//Height update
+            div_update(divs[i],div_sizes[i],"#ce1212");//Height update
+            div_update(divs[i+1],div_sizes[i+1],"#ce1212");//Height update
     
-            div_update(divs[i],div_sizes[i],"blue");//Color update
+            div_update(divs[i],div_sizes[i],"#00adb5");//Color update
             if(i==(j-1))
             {
                 div_update(divs[i+1],div_sizes[i+1],"yellow");//Color update
             }
             else
             {
-                div_update(divs[i+1],div_sizes[i+1],"blue");//Color update
+                div_update(divs[i+1],div_sizes[i+1],"#00adb5");//Color update
             }
             i-=1;
         }
@@ -33,10 +33,10 @@ function Insertion()
 
         for(var t=0;t<j;t++)
         {
-            div_update(divs[t],div_sizes[t],"green");//Color update
+            div_update(divs[t],div_sizes[t],"#4aa96c");//Color update
         }
     }
-    div_update(divs[j-1],div_sizes[j-1],"green");//Color update
+    div_update(divs[j-1],div_sizes[j-1],"#4aa96c");//Color update
 
     enable_buttons();
 }
