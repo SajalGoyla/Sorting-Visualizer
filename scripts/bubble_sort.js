bab6ad1de7da2bb261507d586f38733e2b05118d
Shambhawi Bhure <63147153+ShambhawiBhure@users.noreply.github.com>
2021-05-23 14:03:37 +0530

    Update bubble_sort.js

--- scripts/bubble_sort.js
+++ scripts/bubble_sort.js
@@ -6,25 +6,25 @@ function Bubble()
     {
         for(var j=0;j<array_size-i-1;j++)
         {
-            div_update(divs[j],div_sizes[j],"yellow");//Color update
+            div_update(divs[j],div_sizes[j],"#ffcc29");//Color update
 
             if(div_sizes[j]>div_sizes[j+1])
             {
-                div_update(divs[j],div_sizes[j], "red");//Color update
-                div_update(divs[j+1],div_sizes[j+1], "red");//Color update
+                div_update(divs[j],div_sizes[j], "#ce1212");//Color update
+                div_update(divs[j+1],div_sizes[j+1], "#ce1212");//Color update
 
                 var temp=div_sizes[j];
                 div_sizes[j]=div_sizes[j+1];
                 div_sizes[j+1]=temp;
 
-                div_update(divs[j],div_sizes[j], "red");//Height update
-                div_update(divs[j+1],div_sizes[j+1], "red");//Height update
+                div_update(divs[j],div_sizes[j], "#ce1212");//Height update
+                div_update(divs[j+1],div_sizes[j+1], "#ce1212");//Height update
             }
-            div_update(divs[j],div_sizes[j], "blue");//Color updat
+            div_update(divs[j],div_sizes[j], "#00adb5");//Color update
         }
-        div_update(divs[j],div_sizes[j], "green");//Color update
+        div_update(divs[j],div_sizes[j], "#4aa96c");//Color update
     }
-    div_update(divs[0],div_sizes[0], "green");//Color update
+    div_update(divs[0],div_sizes[0], "#4aa96c");//Color update
 
     enable_buttons();
 }
