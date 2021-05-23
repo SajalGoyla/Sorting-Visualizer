3ddf830e071afdb71c9c035d57f5c36dabc89b9c
Shambhawi Bhure <63147153+ShambhawiBhure@users.noreply.github.com>
2021-05-23 14:08:50 +0530

    Update selection_sort.js

--- scripts/selection_sort.js
+++ scripts/selection_sort.js
@@ -4,26 +4,26 @@ function Selection_sort()
 
     for(var i=0;i<array_size-1;i++)
     {
-        div_update(divs[i],div_sizes[i],"red");//Color update
+        div_update(divs[i],div_sizes[i],"#ce1212");//Color update
 
         index_min=i;
 
         for(var j=i+1;j<array_size;j++)
         {
-            div_update(divs[j],div_sizes[j],"yellow");//Color update
+            div_update(divs[j],div_sizes[j],"#ffcc29");//Color update
 
             if(div_sizes[j]<div_sizes[index_min])
             {
                 if(index_min!=i)
                 {
-                    div_update(divs[index_min],div_sizes[index_min],"blue");//Color update
+                    div_update(divs[index_min],div_sizes[index_min],"#00adb5");//Color update
                 }
                 index_min=j;
-                div_update(divs[index_min],div_sizes[index_min],"red");//Color update
+                div_update(divs[index_min],div_sizes[index_min],"#ce1212");//Color update
             }
             else
             {
-                div_update(divs[j],div_sizes[j],"blue");//Color update
+                div_update(divs[j],div_sizes[j],"#00adb5");//Color update
             }
         }
         
@@ -33,13 +33,13 @@ function Selection_sort()
             div_sizes[index_min]=div_sizes[i];
             div_sizes[i]=temp;
 
-            div_update(divs[index_min],div_sizes[index_min],"red");//Height update
-            div_update(divs[i],div_sizes[i],"red");//Height update
-            div_update(divs[index_min],div_sizes[index_min],"blue");//Color update
+            div_update(divs[index_min],div_sizes[index_min],"#ce1212");//Height update
+            div_update(divs[i],div_sizes[i],"#ce1212");//Height update
+            div_update(divs[index_min],div_sizes[index_min],"b#00adb5");//Color update
         }
-        div_update(divs[i],div_sizes[i],"green");//Color update
+        div_update(divs[i],div_sizes[i],"#4aa96c");//Color update
     }
-    div_update(divs[i],div_sizes[i],"green");//Color update
+    div_update(divs[i],div_sizes[i],"#4aa96c");//Color update
 
     enable_buttons();
 }
