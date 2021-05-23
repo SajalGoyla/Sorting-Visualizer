72784fbd9b98aa61855d5ba41c5aa846ab7451da
Shambhawi Bhure <63147153+ShambhawiBhure@users.noreply.github.com>
2021-05-23 14:08:16 +0530

    Update quick_sort.js

--- scripts/quick_sort.js
+++ scripts/quick_sort.js
@@ -11,44 +11,44 @@ function quick_partition (start, end)
 {
     var i = start + 1;
     var piv = div_sizes[start] ;//make the first element as pivot element.
-    div_update(divs[start],div_sizes[start],"yellow");//Color update
+    div_update(divs[start],div_sizes[start],"#ffcc29");//Color update
 
         for(var j =start + 1; j <= end ; j++ )
         {
             //re-arrange the array by putting elements which are less than pivot on one side and which are greater that on other.
             if (div_sizes[ j ] < piv)
             {
-                div_update(divs[j],div_sizes[j],"yellow");//Color update
+                div_update(divs[j],div_sizes[j],"#ffcc29");//Color update
 
-                div_update(divs[i],div_sizes[i],"red");//Color update
-                div_update(divs[j],div_sizes[j],"red");//Color update
+                div_update(divs[i],div_sizes[i],"#ce1212");//Color update
+                div_update(divs[j],div_sizes[j],"#ce1212");//Color update
 
                 var temp=div_sizes[i];
                 div_sizes[i]=div_sizes[j];
                 div_sizes[j]=temp;
 
-                div_update(divs[i],div_sizes[i],"red");//Height update
-                div_update(divs[j],div_sizes[j],"red");//Height update
+                div_update(divs[i],div_sizes[i],"#ce1212");//Height update
+                div_update(divs[j],div_sizes[j],"#ce1212");//Height update
 
-                div_update(divs[i],div_sizes[i],"blue");//Height update
-                div_update(divs[j],div_sizes[j],"blue");//Height update
+                div_update(divs[i],div_sizes[i],"#00adb5");//Height update
+                div_update(divs[j],div_sizes[j],"#00adb5"); //Height update
 
                 i += 1;
             }
     }
-    div_update(divs[start],div_sizes[start],"red");//Color update
-    div_update(divs[i-1],div_sizes[i-1],"red");//Color update
+    div_update(divs[start],div_sizes[start],"#ce1212");//Color update
+    div_update(divs[i-1],div_sizes[i-1],"#ce1212");//Color update
     
     var temp=div_sizes[start];//put the pivot element in its proper place.
     div_sizes[start]=div_sizes[i-1];
     div_sizes[i-1]=temp;
 
-    div_update(divs[start],div_sizes[start],"red");//Height update
-    div_update(divs[i-1],div_sizes[i-1],"red");//Height update
+    div_update(divs[start],div_sizes[start],"#ce1212");//Height update
+    div_update(divs[i-1],div_sizes[i-1],"#ce1212");//Height update
 
     for(var t=start;t<=i;t++)
     {
-        div_update(divs[t],div_sizes[t],"green");//Color update
+        div_update(divs[t],div_sizes[t],"#4aa96c");//Color update
     }
 
     return i-1;//return the position of the pivot
