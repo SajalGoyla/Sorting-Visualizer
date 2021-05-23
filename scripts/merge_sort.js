08a0e8ed5bf2d7113d8481f658c7c9dbec5f5388
Shambhawi Bhure <63147153+ShambhawiBhure@users.noreply.github.com>
2021-05-23 14:07:26 +0530

    Update merge_sort.js

--- scripts/merge_sort.js
+++ scripts/merge_sort.js
@@ -2,7 +2,7 @@ function Merge()
 {
     c_delay=0;
 
-    merge_partition(0,array_size-1);
+    merge(0,array_size-1);
 
     enable_buttons();
 }
@@ -18,41 +18,41 @@ function merge_sort(start,mid,end)
         if(p>mid)
         {
             Arr[k++]=div_sizes[q++];
-            div_update(divs[q-1],div_sizes[q-1],"red");//Color update
+            div_update(divs[q-1],div_sizes[q-1],"#ce1212");//Color update
         }
         else if(q>end)
         {
             Arr[k++]=div_sizes[p++];
-            div_update(divs[p-1],div_sizes[p-1],"red");//Color update
+            div_update(divs[p-1],div_sizes[p-1],"#ce1212");//Color update
         }
         else if(div_sizes[p]<div_sizes[q])
         {
             Arr[k++]=div_sizes[p++];
-            div_update(divs[p-1],div_sizes[p-1],"red");//Color update
+            div_update(divs[p-1],div_sizes[p-1],"#ce1212");//Color update
         }
         else
         {
             Arr[k++]=div_sizes[q++];
-            div_update(divs[q-1],div_sizes[q-1],"red");//Color update
+            div_update(divs[q-1],div_sizes[q-1],"#ce1212");//Color update
         }
     }
 
     for(var t=0;t<k;t++)
     {
         div_sizes[start++]=Arr[t];
-        div_update(divs[start-1],div_sizes[start-1],"green");//Color update
+        div_update(divs[start-1],div_sizes[start-1],"#4aa96c");//Color update
     }
 }
 
-function merge_partition(start,end)
+function merge(start,end)
 {
     if(start < end)
     {
         var mid=Math.floor((start + end) / 2);
-        div_update(divs[mid],div_sizes[mid],"yellow");//Color update
+        div_update(divs[mid],div_sizes[mid],"#ffcc29");//Color update
 
-        merge_partition(start,mid);
-        merge_partition(mid+1,end);
+        merge(start,mid);
+        merge(mid+1,end);
 
         merge_sort(start,mid,end);
     }
